c450fb087a4b7dc99ec78dee38ea2be619ef01b6
Christopher Chow <christopher.chow@kinesis.org>
2014-05-02 14:50:30 +1000

    Initial commit.

---
+++ metadata.rb
@@ -0,0 +1,11 @@
+name              "ruby"
+maintainer        "Christopher Chow"
+maintainer_email  "chris@chowie@.net"
+license           "Apache 2.0"
+description       "Installs system Ruby from source"
+version           "1.0.0"
+recipe            "ruby", "Installs system Ruby from Brightbox PPA."
+
+depends           "apt"
+
+supports "ubuntu"
---
+++ recipes/default.rb
@@ -0,0 +1,59 @@
+#
+# Cookbook Name:: ruby
+# Recipe:: default
+#
+# Author:: Christopher Chow (<chris@chowie.net>)
+#
+# Copyright 2014, Christopher Chow
+#
+# Licensed under the Apache License, Version 2.0 (the "License");
+# you may not use this file except in compliance with the License.
+# You may obtain a copy of the License at
+#
+#     http://www.apache.org/licenses/LICENSE-2.0
+#
+# Unless required by applicable law or agreed to in writing, software
+# distributed under the License is distributed on an "AS IS" BASIS,
+# WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+# See the License for the specific language governing permissions and
+# limitations under the License.
+#
+
+apt_repository "brightbox-ruby-ng" do
+  uri          "http://ppa.launchpad.net/brightbox/ruby-ng/ubuntu"
+  distribution node["lsb"]["codename"]
+  components   ["main"]
+  keyserver    "keyserver.ubuntu.com"
+  key          "C3173AA6"
+  action       :add
+  notifies     :run, "execute[apt-get update]", :immediately
+end
+
+["build-essential", "ruby2.1", "ruby-switch"].each do |name|
+  apt_package name do
+    action :upgrade
+  end
+end
+
+execute "ruby-switch --set #{node[:ruby][:version]}" do
+  action :run
+  not_if "ruby-switch --check | grep -q '#{node[:ruby][:version]}'"
+end
+
+cookbook_file "/etc/gemrc" do
+  action :create_if_missing
+  source "gemrc"
+  mode   "0644"
+end
+
+["bundler", "rake", "rubygems-bundler"].each do |gem|
+  gem_package gem do
+    action :upgrade
+  end
+end
+
+# Regenerate the binstubs for rubygems-bundler.
+execute "gem regenerate_binstubs" do
+  action :nothing
+  subscribes :run, resources("gem_package[rubygems-bundler]")
+end
